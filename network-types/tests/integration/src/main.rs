46c1a29f2b1d2817f6e33fc48cf97469b21e9fbc
Veronica Manchola <veronica@elastiflow.com>
2025-07-25 12:16:42 -0400

    feat(ENG-12): IPv4 header support (#12)
    * Ipv4 test
    
    * format adjustments

--- network-types/tests/integration/src/main.rs
+++ network-types/tests/integration/src/main.rs
@@ -1,11 +1,11 @@
 mod eth;
+mod ipv4;
 mod utils;
 
 // Import the helper functions and macros
-use crate::eth::{
-    create_eth_test_packet,
-    verify_eth_header,
-    // Additional helper functions would be imported here
+use crate::{
+    eth::{create_eth_test_packet, verify_eth_header},
+    ipv4::{create_ipv4_test_packet, verify_ipv4_header},
 };
 
 fn main() {
@@ -19,4 +19,12 @@ define_header_test!(
     PacketType::Eth,
     create_eth_test_packet,
     verify_eth_header
+);
+
+define_header_test!(
+    test_parses_ipv4_header,
+    Ipv4Hdr,
+    PacketType::Ipv4,
+    create_ipv4_test_packet,
+    verify_ipv4_header
 );
