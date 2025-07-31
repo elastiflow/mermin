15aacac4b5d7ad5096df0509c24c46d3ba96fca0
Sven Cowart <svencowart@gmail.com>
2025-07-31 09:38:50 -0700

    feat(eng-18): simplify PacketMeta and write to PACKETS ring buffer (#17)
    * feat(eng-18): simplify `PacketMeta` and write to PACKETS ring buffer
    
    Streamlines the shared data structures by:
    * Removing the `CReprIpAddr` union and its helpers in favor of a cleaner `PacketMeta` struct.
    * Introducing an `IpAddrType` enum within `PacketMeta` to safely distinguish between IPv4 and IPv6 addresses.
    * Updating the eBPF parser to populate the simplified `PacketMeta` struct directly, removing intermediate fields.
    * Write packet meta field to PACKETS ring buffer
    
    * test(eng-18): parser testing

--- mermin-ebpf/src/main.rs
+++ mermin-ebpf/src/main.rs
@@ -1,18 +1,27 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::bindings::TC_ACT_PIPE;
-use aya_ebpf::macros::classifier;
-use aya_ebpf::programs::TcContext;
+use aya_ebpf::{
+    bindings::TC_ACT_PIPE,
+    macros::{classifier, map},
+    maps::RingBuf,
+    programs::TcContext,
+};
 use aya_log_ebpf::{debug, error, warn};
-use mermin_common::CReprIpAddr;
-use network_types::eth::{EthHdr, EtherType};
-use network_types::ip::{IpProto, Ipv4Hdr, Ipv6Hdr};
-use network_types::tcp::TcpHdr;
-use network_types::udp::UdpHdr;
+use mermin_common::PacketMeta;
+use network_types::{
+    eth::{EthHdr, EtherType},
+    ip::{IpProto, Ipv4Hdr, Ipv6Hdr},
+    tcp::TcpHdr,
+    udp::UdpHdr,
+};
+
+// todo: verify buffer size
+#[map]
+static mut PACKETS: RingBuf = RingBuf::with_byte_size(256 * 1024, 0); // 256 KB
 
 // Defines what kind of header we expect to process in the current iteration.
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum HeaderType {
     Ethernet,
     Ipv4,
@@ -28,34 +37,25 @@ struct Parser {
     // The header-type to parse next at 'offset'
     next_hdr: HeaderType,
 
-    // Information for building the FlowKey (prioritizes innermost headers)
+    // Information for building flow records (prioritizes innermost headers).
     // These fields will be updated as we parse deeper or encounter encapsulations.
-    src_ip_addr: Option<CReprIpAddr>,
-    dst_ip_addr: Option<CReprIpAddr>,
-    src_port: Option<u16>,
-    dst_port: Option<u16>,
-    proto: Option<u8>,     // The innermost L4 protocol number (e.g., 6 for TCP)
-    l3_octet_count: u32, // Total number of octets from the start of the innermost L3 (IP) header to the end of the packet
+    packet_meta: PacketMeta,
 }
 
 impl Parser {
+    // todo(eng-18): consider using default trait instead of new
     fn new() -> Self {
         Parser {
             offset: 0,
             next_hdr: HeaderType::Ethernet,
-            src_ip_addr: None,
-            dst_ip_addr: None,
-            src_port: None,
-            dst_port: None,
-            proto: None,
-            l3_octet_count: 0,
+            packet_meta: PacketMeta::default(),
         }
     }
 
     // Calculate the L3 octet count (from current offset to end of packet)
     // This should be called at the start of L3 (IP) header parsing
-    fn calculate_l3_octet_count(&mut self, ctx: &TcContext) {
-        self.l3_octet_count = ctx.len() - self.offset as u32;
+    fn calc_l3_octet_count(&mut self, packet_len: u32) {
+        self.packet_meta.l3_octet_count = packet_len - self.offset as u32;
     }
 }
 
@@ -95,8 +95,13 @@ fn try_mermin(ctx: TcContext) -> Result<i32, ()> {
         }
     }
 
-    let mut packet_meta = PacketMeta::default();
-    packet_meta.l3_octet_count = parser.l3_octet_count;
+    unsafe {
+        #[allow(static_mut_refs)]
+        let result = PACKETS.output(&parser.packet_meta, 0);
+        if result.is_err() {
+            error!(&ctx, "mermin: failed to write packet to ring buffer");
+        }
+    }
 
     Ok(TC_ACT_PIPE)
 }
@@ -162,7 +167,7 @@ fn parse_ipv4_header(ctx: &TcContext, parser: &mut Parser) -> Result<(), ()> {
         // basic sanity check
         return Err(());
     }
-    parser.calculate_l3_octet_count(ctx);
+    parser.calc_l3_octet_count(ctx.len());
     parser.offset += h_len;
 
     // todo: Extract additional fields from ipv4_hdr
@@ -172,9 +177,9 @@ fn parse_ipv4_header(ctx: &TcContext, parser: &mut Parser) -> Result<(), ()> {
         IpProto::Tcp | IpProto::Udp => {
             // payload headers
             // policy: innermost IP header determines the flow IPs
-            parser.src_ip_addr = Some(CReprIpAddr::new_v4(u32::from_be_bytes(ipv4_hdr.src_addr)));
-            parser.dst_ip_addr = Some(CReprIpAddr::new_v4(u32::from_be_bytes(ipv4_hdr.dst_addr)));
-            parser.proto = Some(next_hdr as u8);
+            parser.packet_meta.src_ipv4_addr = ipv4_hdr.src_addr;
+            parser.packet_meta.dst_ipv4_addr = ipv4_hdr.dst_addr;
+            parser.packet_meta.proto = next_hdr as u8;
             parser.next_hdr = HeaderType::Proto(next_hdr);
         }
         _ => {
@@ -217,7 +222,7 @@ fn parse_ipv4_header(ctx: &TcContext, parser: &mut Parser) -> Result<(), ()> {
 ///  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 fn parse_ipv6_header(ctx: &TcContext, parser: &mut Parser) -> Result<(), ()> {
     let ipv6_hdr: Ipv6Hdr = ctx.load(parser.offset).map_err(|_| ())?;
-    parser.calculate_l3_octet_count(ctx);
+    parser.calc_l3_octet_count(ctx.len());
     parser.offset += Ipv6Hdr::LEN;
 
     let next_hdr = ipv6_hdr.next_hdr;
@@ -225,9 +230,9 @@ fn parse_ipv6_header(ctx: &TcContext, parser: &mut Parser) -> Result<(), ()> {
         IpProto::Tcp | IpProto::Udp => {
             // payload headers
             // policy: innermost IP header determines the flow IPs
-            parser.src_ip_addr = Some(CReprIpAddr::new_v6(ipv6_hdr.src_addr));
-            parser.dst_ip_addr = Some(CReprIpAddr::new_v6(ipv6_hdr.dst_addr));
-            parser.proto = Some(next_hdr as u8);
+            parser.packet_meta.src_ipv6_addr = ipv6_hdr.src_addr;
+            parser.packet_meta.dst_ipv6_addr = ipv6_hdr.dst_addr;
+            parser.packet_meta.proto = next_hdr as u8;
             parser.next_hdr = HeaderType::Proto(next_hdr);
         }
         IpProto::HopOpt
@@ -238,14 +243,14 @@ fn parse_ipv6_header(ctx: &TcContext, parser: &mut Parser) -> Result<(), ()> {
         | IpProto::Hip
         | IpProto::Shim6 => {
             // ipv6 extension headers
-            parser.src_ip_addr = Some(CReprIpAddr::new_v6(ipv6_hdr.src_addr));
-            parser.dst_ip_addr = Some(CReprIpAddr::new_v6(ipv6_hdr.dst_addr));
+            parser.packet_meta.src_ipv6_addr = ipv6_hdr.src_addr;
+            parser.packet_meta.dst_ipv6_addr = ipv6_hdr.dst_addr;
             parser.next_hdr = HeaderType::Proto(next_hdr);
         }
         IpProto::Ipv6NoNxt => {
             // ipv6 no next header
             parser.next_hdr = HeaderType::StopProcessing;
-            parser.proto = Some(next_hdr as u8);
+            parser.packet_meta.proto = next_hdr as u8;
         }
         _ => {
             warn!(
@@ -290,8 +295,8 @@ fn parse_tcp_header(ctx: &TcContext, parser: &mut Parser) -> Result<(), ()> {
     let tcp_hdr: TcpHdr = ctx.load(parser.offset).map_err(|_| ())?;
     parser.offset += TcpHdr::LEN;
 
-    parser.src_port = Option::from(tcp_hdr.src_port());
-    parser.dst_port = Option::from(tcp_hdr.dst_port());
+    parser.packet_meta.src_port = tcp_hdr.src;
+    parser.packet_meta.dst_port = tcp_hdr.dst;
     // TODO: extract and assign additional tcp fields
 
     Ok(())
@@ -314,8 +319,8 @@ fn parse_udp_header(ctx: &TcContext, parser: &mut Parser) -> Result<(), ()> {
     let udp_hdr: UdpHdr = ctx.load(parser.offset).map_err(|_| ())?;
     parser.offset += UdpHdr::LEN;
 
-    parser.src_port = Option::from(udp_hdr.src_port());
-    parser.dst_port = Option::from(udp_hdr.dst_port());
+    parser.packet_meta.src_port = udp_hdr.src;
+    parser.packet_meta.dst_port = udp_hdr.dst;
     // TODO: extract and assign additional tcp fields
 
     Ok(())
@@ -325,6 +330,301 @@ fn parse_udp_header(ctx: &TcContext, parser: &mut Parser) -> Result<(), ()> {
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate alloc;
+
+    use alloc::vec::Vec;
+    use core::mem;
+
+    use super::*;
+
+    // Mock TcContext for testing
+    struct MockTcContext {
+        data: Vec<u8>,
+    }
+
+    impl MockTcContext {
+        fn new(data: Vec<u8>) -> Self {
+            Self { data }
+        }
+
+        #[allow(unused)]
+        fn len(&self) -> u32 {
+            self.data.len() as u32
+        }
+
+        #[allow(unused)]
+        fn load<T>(&self, offset: usize) -> Result<T, ()>
+        where
+            T: Copy,
+        {
+            if offset + mem::size_of::<T>() > self.data.len() {
+                return Err(());
+            }
+
+            let ptr = unsafe { self.data.as_ptr().add(offset) as *const T };
+            let value = unsafe { *ptr };
+            Ok(value)
+        }
+    }
+
+    // Helper function to create an Ethernet header test packet
+    fn create_eth_test_packet() -> Vec<u8> {
+        let mut packet = Vec::new();
+
+        // Destination MAC (ff:ff:ff:ff:ff:ff)
+        packet.extend_from_slice(&[0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
+        // Source MAC (00:11:22:33:44:55)
+        packet.extend_from_slice(&[0x00, 0x11, 0x22, 0x33, 0x44, 0x55]);
+        // EtherType (0x0800, big-endian for IPv4)
+        packet.extend_from_slice(&[0x08, 0x00]);
+
+        packet
+    }
+
+    // Helper function to create an IPv4 header test packet
+    fn create_ipv4_test_packet() -> Vec<u8> {
+        let mut packet = Vec::new();
+
+        // Version (4) and IHL (5) = 0x45
+        packet.push(0x45);
+        // DSCP and ECN
+        packet.push(0x00);
+        // Total Length (20 bytes for header)
+        packet.extend_from_slice(&[0x00, 0x14]);
+        // Identification
+        packet.extend_from_slice(&[0x00, 0x00]);
+        // Flags and Fragment Offset
+        packet.extend_from_slice(&[0x00, 0x00]);
+        // TTL
+        packet.push(0x40);
+        // Protocol (TCP = 6)
+        packet.push(0x06);
+        // Header Checksum
+        packet.extend_from_slice(&[0x00, 0x00]);
+        // Source IP (192.168.1.1)
+        packet.extend_from_slice(&[0xc0, 0xa8, 0x01, 0x01]);
+        // Destination IP (192.168.1.2)
+        packet.extend_from_slice(&[0xc0, 0xa8, 0x01, 0x02]);
+
+        packet
+    }
+
+    // Helper function to create an IPv6 header test packet
+    fn create_ipv6_test_packet() -> Vec<u8> {
+        let mut packet = Vec::new();
+
+        // Version (6), Traffic Class, Flow Label
+        packet.extend_from_slice(&[0x60, 0x00, 0x00, 0x00]);
+        // Payload Length
+        packet.extend_from_slice(&[0x00, 0x00]);
+        // Next Header (TCP = 6)
+        packet.push(0x06);
+        // Hop Limit
+        packet.push(0x40);
+        // Source IP (2001:db8::1)
+        packet.extend_from_slice(&[
+            0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x01,
+        ]);
+        // Destination IP (2001:db8::2)
+        packet.extend_from_slice(&[
+            0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x02,
+        ]);
+
+        packet
+    }
+
+    // Helper function to create a TCP header test packet
+    fn create_tcp_test_packet() -> Vec<u8> {
+        let mut packet = Vec::new();
+
+        // Source Port (12345)
+        packet.extend_from_slice(&[0x30, 0x39]);
+        // Destination Port (80)
+        packet.extend_from_slice(&[0x00, 0x50]);
+        // Sequence Number
+        packet.extend_from_slice(&[0x00, 0x00, 0x00, 0x00]);
+        // Acknowledgment Number
+        packet.extend_from_slice(&[0x00, 0x00, 0x00, 0x00]);
+        // Data Offset (5), Reserved, Flags (SYN)
+        packet.extend_from_slice(&[0x50, 0x02]);
+        // Window Size
+        packet.extend_from_slice(&[0x20, 0x00]);
+        // Checksum
+        packet.extend_from_slice(&[0x00, 0x00]);
+        // Urgent Pointer
+        packet.extend_from_slice(&[0x00, 0x00]);
+
+        packet
+    }
+
+    // Helper function to create a UDP header test packet
+    fn create_udp_test_packet() -> Vec<u8> {
+        let mut packet = Vec::new();
+
+        // Source Port (12345)
+        packet.extend_from_slice(&[0x30, 0x39]);
+        // Destination Port (53)
+        packet.extend_from_slice(&[0x00, 0x35]);
+        // Length (8 bytes for header)
+        packet.extend_from_slice(&[0x00, 0x08]);
+        // Checksum
+        packet.extend_from_slice(&[0x00, 0x00]);
+
+        packet
+    }
+
+    // #[test]
+    // fn test_my_tc_program() {
+    //     let mock_packet_data = vec![0xDE, 0xAD, 0xBE, 0xEF];
+    //     let ctx = MockTcContext::new(mock_packet_data);
+    //
+    //     // Call your eBPF program's main function with the mock context
+    //     let result = mermin(&ctx as *const _ as *mut _);
+    //
+    //     // Assert on the expected outcome of the program
+    //     assert_eq!(result, 0); // Or whatever your program should return
+    // }
+
+    // Test Parser initialization
+    #[test]
+    fn test_parser_initialization() {
+        let parser = Parser::new();
+
+        assert_eq!(parser.offset, 0);
+        assert!(matches!(parser.next_hdr, HeaderType::Ethernet));
+
+        // Check that packet_meta is initialized with default values
+        let packet_meta = parser.packet_meta;
+        assert_eq!(packet_meta.src_ipv4_addr, [0, 0, 0, 0]);
+        assert_eq!(packet_meta.dst_ipv4_addr, [0, 0, 0, 0]);
+        assert_eq!(packet_meta.src_port, [0, 0]);
+        assert_eq!(packet_meta.dst_port, [0, 0]);
+    }
+
+    #[test]
+    fn test_parser_calculate_l3_octet_count() {
+        let mut parser = Parser::new();
+
+        parser.offset = 32;
+        parser.calc_l3_octet_count(256);
+
+        assert_eq!(parser.packet_meta.l3_octet_count, 224);
+    }
+
+    // Test parse_ethernet_header function
+    #[test]
+    fn test_parse_ethernet_header() {
+        let mut parser = Parser::new();
+        let packet = create_eth_test_packet();
+        let ctx = MockTcContext::new(packet);
+
+        let result = parse_ethernet_header(unsafe { *(&ctx as *const _ as *mut _) }, &mut parser);
+
+        assert!(result.is_ok());
+        assert_eq!(parser.offset, EthHdr::LEN);
+        assert!(matches!(parser.next_hdr, HeaderType::Ipv4));
+    }
+
+    // Test parse_ipv4_header function
+    #[test]
+    fn test_parse_ipv4_header() {
+        let mut parser = Parser::new();
+        parser.next_hdr = HeaderType::Ipv4;
+        let packet = create_ipv4_test_packet();
+        let ctx = MockTcContext::new(packet);
+
+        let result = parse_ipv4_header(unsafe { *(&ctx as *const _ as *mut _) }, &mut parser);
+
+        assert!(result.is_ok());
+        assert_eq!(parser.offset, 20); // IPv4 header length (5 * 4 bytes)
+        assert!(matches!(parser.next_hdr, HeaderType::Proto(IpProto::Tcp)));
+        assert_eq!(parser.packet_meta.src_ipv4_addr, [0xc0, 0xa8, 0x01, 0x01]); // 192.168.1.1
+        assert_eq!(parser.packet_meta.dst_ipv4_addr, [0xc0, 0xa8, 0x01, 0x02]); // 192.168.1.2
+        assert_eq!(parser.packet_meta.proto, 6); // TCP
+    }
+
+    // Test parse_ipv4_header function with invalid header length
+    #[test]
+    fn test_parse_ipv4_header_invalid_length() {
+        let mut parser = Parser::new();
+        parser.next_hdr = HeaderType::Ipv4;
+        let mut packet = create_ipv4_test_packet();
+        // Change IHL to invalid value (0)
+        packet[0] = 0x40; // Version 4, IHL 0
+        let ctx = MockTcContext::new(packet);
+
+        let result = parse_ipv4_header(unsafe { *(&ctx as *const _ as *mut _) }, &mut parser);
+
+        assert!(result.is_err());
+    }
+
+    // Test parse_ipv6_header function
+    #[test]
+    fn test_parse_ipv6_header() {
+        let mut parser = Parser::new();
+        parser.next_hdr = HeaderType::Ipv6;
+        let packet = create_ipv6_test_packet();
+        let ctx = MockTcContext::new(packet);
+
+        let result = parse_ipv6_header(unsafe { *(&ctx as *const _ as *mut _) }, &mut parser);
+
+        assert!(result.is_ok());
+        assert_eq!(parser.offset, Ipv6Hdr::LEN);
+        assert!(matches!(parser.next_hdr, HeaderType::Proto(IpProto::Tcp)));
+        assert_eq!(
+            parser.packet_meta.src_ipv6_addr,
+            [
+                0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x01
+            ]
+        ); // 2001:db8::1
+        assert_eq!(
+            parser.packet_meta.dst_ipv6_addr,
+            [
+                0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                0x00, 0x02
+            ]
+        ); // 2001:db8::2
+        assert_eq!(parser.packet_meta.proto, 6); // TCP
+    }
+
+    // Test parse_tcp_header function
+    #[test]
+    fn test_parse_tcp_header() {
+        let mut parser = Parser::new();
+        parser.next_hdr = HeaderType::Proto(IpProto::Tcp);
+        let packet = create_tcp_test_packet();
+        let ctx = MockTcContext::new(packet);
+
+        let result = parse_tcp_header(unsafe { *(&ctx as *const _ as *mut _) }, &mut parser);
+
+        assert!(result.is_ok());
+        assert_eq!(parser.offset, TcpHdr::LEN);
+        assert_eq!(parser.packet_meta.src_port, [0x30, 0x39]); // 12345
+        assert_eq!(parser.packet_meta.dst_port, [0x00, 0x50]); // 80
+    }
+
+    // Test parse_udp_header function
+    #[test]
+    fn test_parse_udp_header() {
+        let mut parser = Parser::new();
+        parser.next_hdr = HeaderType::Proto(IpProto::Udp);
+        let packet = create_udp_test_packet();
+        let ctx = MockTcContext::new(packet);
+
+        let result = parse_udp_header(unsafe { *(&ctx as *const _ as *mut _) }, &mut parser);
+
+        assert!(result.is_ok());
+        assert_eq!(parser.offset, UdpHdr::LEN);
+        assert_eq!(parser.packet_meta.src_port, [0x30, 0x39]); // 12345
+        assert_eq!(parser.packet_meta.dst_port, [0x00, 0x35]); // 53
+    }
 }
 
 #[link_section = "license"]
