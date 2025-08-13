e14f263138fd6cf26388fec061cd6e9f63f3165a
Sven Cowart <svencowart@gmail.com>
2025-08-13 09:11:01 -0700

    feat: adds runtime mod and hooks into main

---
+++ mermin/src/runtime.rs
@@ -0,0 +1,53 @@
+use std::{error::Error, fmt};
+
+use clap::Parser;
+
+use crate::runtime::{
+    cli::Cli,
+    config::{Config, ConfigError},
+};
+
+pub mod cli;
+pub mod config;
+
+pub struct Runtime {
+    #[allow(dead_code)]
+    pub cli: Cli,
+    pub config: Config,
+}
+
+impl Runtime {
+    pub fn new() -> Result<Self, RuntimeError> {
+        let cli = Cli::parse();
+        let (config, cli) = Config::new(cli)?;
+
+        Ok(Runtime { cli, config })
+    }
+}
+
+#[derive(Debug)]
+pub enum RuntimeError {
+    Config(ConfigError),
+}
+
+impl fmt::Display for RuntimeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            RuntimeError::Config(e) => e.fmt(f),
+        }
+    }
+}
+
+impl Error for RuntimeError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            RuntimeError::Config(e) => Some(e),
+        }
+    }
+}
+
+impl From<ConfigError> for RuntimeError {
+    fn from(e: ConfigError) -> Self {
+        RuntimeError::Config(e)
+    }
+}
