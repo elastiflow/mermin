1f5ae141e49bca5af29b0bd9ce7779777abb755e
ElastiJAM <james@elastiflow.com>
2025-10-03 21:34:13 -0400

    chore: more formatting cleanup nits

--- network-types/tests/integration/src/main.rs
+++ network-types/tests/integration/src/main.rs
@@ -1,4 +1,3 @@
-// Refactored modules
 mod ah;
 mod destopts;
 mod esp;
@@ -8,6 +7,8 @@ mod geneve;
 mod gre;
 mod hip;
 mod hop;
+mod ipv4;
+mod ipv6;
 mod mobility;
 mod routing;
 mod shim6;
@@ -15,21 +16,9 @@ mod tcp;
 mod udp;
 mod utils;
 mod vxlan;
-
-// TODO: Uncomment as we refactor each type
-// mod crh;
-// mod geneve;
-// mod gre;
-// mod hip;
-mod ipv4;
-mod ipv6;
-// mod rpl_source_route;
-// mod segment_routing;
-// mod type2;
-// mod vxlan;
 mod wireguard;
 
-// Import the helper functions and macros - only for refactored types
+// Import the helper functions and macros
 use crate::{
     ah::{create_ah_test_packet, verify_ah_header},
     destopts::{create_destopts_test_packet, verify_destopts_header},
@@ -64,75 +53,11 @@ fn main() {
 // Refactored to match new parsing methodology - only tests extracted fields
 
 define_header_test!(
-    test_parses_eth_header,
-    EthernetTestData,
-    PacketType::Eth,
-    create_eth_test_packet,
-    verify_eth_header
-);
-
-define_header_test!(
-    test_parses_ipv4_header,
-    Ipv4TestData,
-    PacketType::Ipv4,
-    create_ipv4_test_packet,
-    verify_ipv4_header
-);
-
-define_header_test!(
-    test_parses_ipv6_header,
-    Ipv6TestData,
-    PacketType::Ipv6,
-    create_ipv6_test_packet,
-    verify_ipv6_header
-);
-
-define_header_test!(
-    test_parses_tcp_header,
-    TcpTestData,
-    PacketType::Tcp,
-    create_tcp_test_packet,
-    verify_tcp_header
-);
-
-define_header_test!(
-    test_parses_udp_header,
-    UdpTestData,
-    PacketType::Udp,
-    create_udp_test_packet,
-    verify_udp_header
-);
-
-define_header_test!(
     test_parses_ah_header,
     AhTestData,
     PacketType::Ah,
     create_ah_test_packet,
     verify_ah_header
-);
-
-define_header_test!(
-    test_parses_esp_header,
-    EspTestData,
-    PacketType::Esp,
-    create_esp_test_packet,
-    verify_esp_header
-);
-
-define_header_test!(
-    test_parses_hop_header,
-    HopOptTestData,
-    PacketType::Hop,
-    create_hop_test_packet,
-    verify_hop_header
-);
-
-define_header_test!(
-    test_parses_fragment_header,
-    FragmentTestData,
-    PacketType::Fragment,
-    create_fragment_test_packet,
-    verify_fragment_header
 );
 
 define_header_test!(
@@ -144,19 +69,27 @@ define_header_test!(
 );
 
 define_header_test!(
-    test_parses_mobility_header,
-    MobilityTestData,
-    PacketType::Mobility,
-    create_mobility_test_packet,
-    verify_mobility_header
+    test_parses_eth_header,
+    EthernetTestData,
+    PacketType::Eth,
+    create_eth_test_packet,
+    verify_eth_header
 );
 
 define_header_test!(
-    test_parses_shim6_header,
-    Shim6TestData,
-    PacketType::Shim6,
-    create_shim6_test_packet,
-    verify_shim6_header
+    test_parses_esp_header,
+    EspTestData,
+    PacketType::Esp,
+    create_esp_test_packet,
+    verify_esp_header
+);
+
+define_header_test!(
+    test_parses_fragment_header,
+    FragmentTestData,
+    PacketType::Fragment,
+    create_fragment_test_packet,
+    verify_fragment_header
 );
 
 define_header_test!(
@@ -176,11 +109,11 @@ define_header_test!(
 );
 
 define_header_test!(
-    test_parses_vxlan_header,
-    VxlanTestData,
-    PacketType::Vxlan,
-    create_vxlan_test_packet,
-    verify_vxlan_header
+    test_parses_gre_header,
+    GreTestData,
+    PacketType::Gre,
+    create_gre_test_packet,
+    verify_gre_header
 );
 
 define_header_test!(
@@ -192,11 +125,75 @@ define_header_test!(
 );
 
 define_header_test!(
-    test_parses_gre_header,
-    GreTestData,
-    PacketType::Gre,
-    create_gre_test_packet,
-    verify_gre_header
+    test_parses_hop_header,
+    HopOptTestData,
+    PacketType::Hop,
+    create_hop_test_packet,
+    verify_hop_header
+);
+
+define_header_test!(
+    test_parses_ipv4_header,
+    Ipv4TestData,
+    PacketType::Ipv4,
+    create_ipv4_test_packet,
+    verify_ipv4_header
+);
+
+define_header_test!(
+    test_parses_ipv6_header,
+    Ipv6TestData,
+    PacketType::Ipv6,
+    create_ipv6_test_packet,
+    verify_ipv6_header
+);
+
+define_header_test!(
+    test_parses_mobility_header,
+    MobilityTestData,
+    PacketType::Mobility,
+    create_mobility_test_packet,
+    verify_mobility_header
+);
+
+define_header_test!(
+    test_parses_shim6_header,
+    Shim6TestData,
+    PacketType::Shim6,
+    create_shim6_test_packet,
+    verify_shim6_header
+);
+
+define_header_test!(
+    test_parses_tcp_header,
+    TcpTestData,
+    PacketType::Tcp,
+    create_tcp_test_packet,
+    verify_tcp_header
+);
+
+define_header_test!(
+    test_parses_udp_header,
+    UdpTestData,
+    PacketType::Udp,
+    create_udp_test_packet,
+    verify_udp_header
+);
+
+define_header_test!(
+    test_parses_vxlan_header,
+    VxlanTestData,
+    PacketType::Vxlan,
+    create_vxlan_test_packet,
+    verify_vxlan_header
+);
+
+define_header_test!(
+    test_parses_wireguard_cookie_reply_header,
+    WireGuardCookieReplyTestData,
+    PacketType::WireGuardCookieReply,
+    create_wireguard_cookie_reply_test_packet,
+    verify_wireguard_cookie_reply_header
 );
 
 define_header_test!(
@@ -213,14 +210,6 @@ define_header_test!(
     PacketType::WireGuardResponse,
     create_wireguard_response_test_packet,
     verify_wireguard_response_header
-);
-
-define_header_test!(
-    test_parses_wireguard_cookie_reply_header,
-    WireGuardCookieReplyTestData,
-    PacketType::WireGuardCookieReply,
-    create_wireguard_cookie_reply_test_packet,
-    verify_wireguard_cookie_reply_header
 );
 
 define_header_test!(
