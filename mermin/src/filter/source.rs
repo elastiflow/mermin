cd38faf75785d2e848d733773a9e1e6337ee14e6
ElastiJAM <james@elastiflow.com>
2026-01-29 20:13:40 -0500

    feat: everybody stay calm

--- mermin/src/filter/source.rs
+++ mermin/src/filter/source.rs
@@ -573,6 +573,9 @@ mod tests {
             reverse_ip_flow_label: 0,
             forward_metadata_seen: 1,
             reverse_metadata_seen: 0,
+            pid: 0,
+            tgid: 0,
+            comm: [0u8; 16],
         }
     }
 
